57d2f23bf2a13f5a3f9a86eda81f466aeb35cd92
Marcos de la Calle <marcos.dlcs@gmail.com>
2021-04-17 22:06:23 +0200

    Add a basic Spring Boot project from initializr

---
+++ rentporium/src/main/java/dev/mdlcs/rentporium/RentporiumApplication.java
@@ -0,0 +1,13 @@
+package dev.mdlcs.rentporium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentporiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentporiumApplication.class, args);
+	}
+
+}
